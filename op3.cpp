08122f0fdd106a4f5d28e0244851d8911a0e7014
vianki707 <guzmanvianca2006@gmail.com>
2025-08-04 01:15:11 -0500

    Sexto commit con codigo base

--- op3.cpp
+++ op3.cpp
@@ -7,7 +7,11 @@ string construirFuncionObjetivo(int precioDeVentaMesa, int precioDeVentaSilla) {
     string funcion = "Z = ";
 
     // Funcion para P1: Precio de Mesas
-    funcion += to_string(precioDeVentaMesa) + "x1";
+        if (precioDeVentaMesa < 0) {
+        funcion += "- " + to_string(abs(precioDeVentaMesa)) + "x1";
+    } else {
+        funcion += to_string(precioDeVentaMesa) + "x1";
+    }
 
     //Funcion para P2: Precio de sillas
     if (precioDeVentaSilla >= 0) {
